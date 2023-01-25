b4ddb8e437aa44fee34974dfe011bc8ccece5829
Faisal Aslam <112485576+Faisii786@users.noreply.github.com>
2023-01-26 04:09:14 +0500

    Fourth Commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,25 +6,57 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
         body: Column(
-      children: [
-        Image.asset("assets/images/login.png"),
-        Text(
-          "Wellcome",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        TextField(
-          decoration: InputDecoration(
-              hintText: "Enter User Name", labelText: "User Name"),
-        ),
-        TextField(
-          decoration: InputDecoration(
-              hintText: "Enter Password", labelText: "Password"),
-        ),
-      ],
-    ));
+          children: [
+            Image.asset(
+              "assets/images/login.png",
+              width: 300,
+            ),
+            Text(
+              "Wellcome",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        hintText: "Enter User Name",
+                        labelText: "User Name"),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        hintText: "Enter Password",
+                        labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
