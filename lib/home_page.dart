82447c374db494472d8d43e47122aca2001e477d
Faisal Aslam <112485576+Faisii786@users.noreply.github.com>
2023-01-26 03:55:10 +0500

    Third Commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,12 +6,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("First App"),
-      ),
-      drawer: Drawer(
-        child: Text("Faisal"),
-      ),
-    );
+        body: Column(
+      children: [
+        Image.asset("assets/images/login.png"),
+        Text(
+          "Wellcome",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        TextField(
+          decoration: InputDecoration(
+              hintText: "Enter User Name", labelText: "User Name"),
+        ),
+        TextField(
+          decoration: InputDecoration(
+              hintText: "Enter Password", labelText: "Password"),
+        ),
+      ],
+    ));
   }
 }
