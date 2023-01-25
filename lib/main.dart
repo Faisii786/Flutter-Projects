3aa0ddf2fc192d3786dff66267f36fa8bd55b546
Faisal Aslam <112485576+Faisii786@users.noreply.github.com>
2023-01-26 03:29:23 +0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/home_Page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,10 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter App",
-      home: Material(
-        child: Text("Faisal Aslam"),
-      ),
+      title: "Flutter First App",
+      home: HomePage(),
     );
   }
 }
